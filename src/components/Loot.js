d94280982a703d11815cebb5c8726a5b04fa9bf7
gogolearn <jay.jongcheol.park@gmail.com>
2017-11-16 18:20:02 -0500

    Stateless Functional Components

--- src/components/Loot.js
+++ src/components/Loot.js
@@ -7,19 +7,18 @@ export class Loot extends Component {
     this.props.fetchBitcoin()
   }
 
+  computeBitcoin = () => {
+    const { bitcoin } = this.props
+    if (Object.keys(bitcoin).length === 0) return ''
+    return this.props.balance / parseInt(bitcoin.bpi.USD.rate.replace(',', ''), 10)
+  }
   render () {
     return (
       <div>
-        <h3>Bitcoin balance:</h3>
+        <h3>Bitcoin balance: {this.computeBitcoin()}</h3>
       </div>
     )
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    state
-  }
-}
-
-export default connect(mapStateToProps, { fetchBitcoin })(Loot)
+export default connect(state => state, { fetchBitcoin })(Loot)
